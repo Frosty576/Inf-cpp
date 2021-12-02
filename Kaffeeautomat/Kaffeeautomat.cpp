2a41dddf20dc6ac0eb2466a89a3cbdfccdb6d8ae
Tom Brown <tom.brown@stud.hawk.de>
2021-12-02 14:35:53 +0100

    Startnmenu erstellt

--- Kaffeeautomat/Kaffeeautomat.cpp
+++ Kaffeeautomat/Kaffeeautomat.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 int main() {
 	system("CLS"); //Bildshirm löschen
+
+	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n";
+	cout << "Bitte w\x84hlen Sie aus : \n";
+	cout << "(k) Kaffee\n";
+	cout << "(e) Espresso\n";
+	cout << "(s) Service-Mode\n";
