f494e4e0724802a9c3999c6838a8545bf2b83409
Tom Brown <tom.brown@stud.hawk.de>
2021-12-09 15:06:09 +0100

    Erste eingabe codiert

---
+++ Projekt2/B.4.1.7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+#include <sstream>;
+using namespace std;
+
+int main() {
+
+	cout << "Bitte ihre Name und Vorname eingeben:" << endl;
+	string name;
+	string vorname;
+	cin >> name >> vorname;
+	cin.clear();
