b5623c837526ad019fab49a1fe7c88d4ff8b65f5
Tom Brown <tom.brown@stud.hawk.de>
2022-01-10 11:56:41 +0100

    Kommentare für B.4.2.2 hinzugefügt

--- B.4.4.2/B.4.4.2.cpp
+++ B.4.4.2/B.4.4.2.cpp
@@ -1,12 +1,12 @@
 int i, k, n;
 
 i = 101, k = 5, n = 10;
-while (i > 0) k = 2 * k; // 
+while (i > 0) k = 2 * k; // Während i größer null ist, wird k mit 2 multipliziert. Da i sich nie ändert, wird der Schleife unendlich ausgeführt.
 
 i = 101, k = 5, n = 10;
-while (i != 0) i = i - 2; // ...
+while (i != 0) i = i - 2; // Während i ungleich 0 ist, wird i - 2 berechnet. Da i mit eine ungerade Zahl anfängt und mit eine gerade Zahl subtrahiert wird, wird es immer ungleich null bleiben. Der Schleife wird sich unendlich lang ausführen.
 
 i = 101, k = 5, n = 10;
-while (n != i) { // ...
+while (n != i) { // Während n ungleich i ist, wird i vorherig inkrementiert und n wird den Wert 2 * i zugewiesen. Da n immer das doppelte von i sein wird, wird n immer ungleich i sein, welche zu eine unendliche Schleife führt.
 	++i;
 	n = 2 * i;
