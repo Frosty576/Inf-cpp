0a83be5611794274eb47c7ce8cc48f326daec5f6
Tom Brown <tom.brown@stud.hawk.de>
2022-01-24 15:06:25 +0100

    Main Funktion und Funktion für die Berechnung von Kuegel Oeberflaeche und Volume codiert

---
+++ B.4.9.2/B.4.9.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+bool berechne_kuegeldaten(double fr, double* po, double* pv) {
+
+	if (fr < 0.0)
+		return false;
+	*po = 4 * M_PI * pow(fr, 2.0);
+	*pv = (4 / 3) * M_PI * pow(fr, 3.0);
+	return true;
+}
+
+int main() {
+	double r;
+	double o;
+	double v;
+	cout << "Radius eingeben.\n";
+	cin >> r;
+	if (berechne_kuegeldaten(r, &o, &v) == true) {
+		cout << "\nOberflaeche der Kuegel = " << o << endl << "Volume der Kuegel = " << v << endl;
+		system("pause");
+	}
+	else {
+		cout << "Eingabe muss Positive sein.\n";
+		system("pause");
+	}
+}
