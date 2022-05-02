734e7c66ff17a1864d8ed241498cafdcf9c7e90c
Tom Brown <tom.brown@stud.hawk.de>
2022-05-02 16:55:29 +0200

    Klasse mit privaten Attributen erstellt

---
+++ Inf2/Auto.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class Auto {
+
+private:
+	int radzahl;
+	std::string farbe;
+	int ps;
+	int anzahl_sitze;
+	float spritverbrauch;
+	float tankinhalt;
+	int tankgroesse;
+public:
+	
+};
