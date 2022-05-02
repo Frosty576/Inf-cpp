a7289d9e9cd193f5e51584abfdaaf716644b9653
Tom Brown <tom.brown@stud.hawk.de>
2022-05-02 17:09:21 +0200

    Testprogramm fuer Konstruktoren erweitert

--- Inf2/Auto.cpp
+++ Inf2/Auto.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "Auto.h"
 
-Auto renault;
+Auto renault{ 4, "rot", 1000, 5, 5.0f, 100.0f, 100 };
 
 int main() {
 
-	Auto bmw;
+	Auto bmw{ 4, "rot", 1000, 5, 5.0f, 50.0f, 100 };
 	
 
-	Auto* renault = new Auto;
-	delete renault;
+	Auto* volkswagen = new Auto{ 4, "blau", 300, 5, 10.0f, 200.0f, 200 };
+	delete volkswagen;
 
 
 
