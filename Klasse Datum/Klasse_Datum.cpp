e84fc8190b1aea9c2ca01670264df53d40eca258
Tom Brown <tom.brown@stud.hawk.de>
2022-05-08 16:40:47 +0200

    Testprogramm versucht 11 Objekte der Klasse zu erzeugen, um Konstruktorwarnung zu testen.

--- Klasse Datum/Klasse_Datum.cpp
+++ Klasse Datum/Klasse_Datum.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "Klasse_Datum.h"
 
+int Datum::KlassenZaehler{0};
 int main() {
 
 	Datum heute{ 2,5,2022 };
-	std::cout << heute.IstSchaltjahr(4) << std::endl;
+	Datum e1 {1, 1, 1};
+	Datum e2{ 1, 1, 1 };
+	Datum e3{ 1, 1, 1 };
+	Datum e4{ 1, 1, 1 };
+	Datum e5{ 1, 1, 1 };
+	Datum e6{ 1, 1, 1 };
+	Datum e7{ 1, 1, 1 };
+	Datum e8{ 1, 1, 1 };
+	Datum e9{ 1, 1, 1 };
+	Datum e10{ 1, 1, 1 };
+	std::cout << Datum::IstSchaltjahr(4) << std::endl;
 	std::cout << heute.IstSchaltjahr(100) << std::endl;
 	std::cout << heute.IstSchaltjahr(400) << std::endl;
 	std::cout << heute.TagDesJahres() << std::endl;
