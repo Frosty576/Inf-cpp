859867ca615331e9525e3c27d02916da6ed9ea56
Tom Brown <tom.brown@stud.hawk.de>
2022-05-08 16:38:05 +0200

    Allgemeine Konstruktor beschwert sich, wenn man versucht mehr als zehn exemplare der Klasse zu erzeugen.

--- Klasse Datum/Klasse_Datum.h
+++ Klasse Datum/Klasse_Datum.h
@@ -6,44 +6,56 @@ private:
 	int tag;
 	int monat;
 	int jahr;
+	int KlassenNummer;
+	static int KlassenZaehler;
 public:
 	Datum() {
 		tag = 0;
 		monat = 0;
 		jahr = 0;
+		KlassenNummer = 0;
 	}
 
 	Datum(int Tag, int Monat, int Jahr) : tag{ Tag }, monat{ Monat }, jahr{ Jahr }
-	{}
+	{
+		if (KlassenZaehler > 9) {
+			std::cout << "Sie versuchen zu viele Objekte der Klasse zu instanzieren" << std::endl;
+			system("pause");
+			return;
+		}
+		KlassenNummer = ++KlassenZaehler;
+		std::cout << KlassenNummer << std::endl;	
+	}
 
 	~Datum()
 	{}
-
-	static int IstSchaltjahr(int irgendeinJahr) {
-		if ((irgendeinJahr % 4 == 0) && (irgendeinJahr % 100 != 0) || (irgendeinJahr % 400 == 0)) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-
-	int TagDesJahres() {
-		IstSchaltjahr(jahr);
-		int tageProMonat[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		int tageProMonatIstSchalt[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		int TagDesJahres = 0;
-		if (IstSchaltjahr(jahr) == 1) {
-			for (int i = 0; i < (monat - 1); i++) {
-				TagDesJahres += tageProMonatIstSchalt[i];
+	
+		static int IstSchaltjahr(int irgendeinJahr) {
+			if ((irgendeinJahr % 4 == 0) && (irgendeinJahr % 100 != 0) || (irgendeinJahr % 400 == 0)) {
+				return 1;
+			}
+			else {
+				return 0;
 			}
 		}
-		if (IstSchaltjahr(jahr) == 0) {
-			for (int i = 0; i < (monat - 1); i++) {
-				TagDesJahres += tageProMonat[i];
+
+		int TagDesJahres() {
+			IstSchaltjahr(jahr);
+			int tagProMonat[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			int tagProMonatIstSchalt[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			int TagDesJahres = 0;
+			if (IstSchaltjahr(jahr) == 1) {
+				for (int i = 0; i < (monat - 1); i++) {
+					TagDesJahres += tagProMonatIstSchalt[i];
+				}
 			}
+			if (IstSchaltjahr(jahr) == 0) {
+				for (int i = 0; i < (monat - 1); i++) {
+					TagDesJahres += tagProMonat[i];
+				}
+			}
+			TagDesJahres = TagDesJahres + tag;
+			return TagDesJahres;
 		}
-		TagDesJahres = TagDesJahres + tag;
-		return TagDesJahres;
-	}
+	
 };
