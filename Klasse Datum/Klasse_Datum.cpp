62e2032140a292deb2f248d204c510236a81ca3b
Tom Brown <tom.brown@stud.hawk.de>
2022-05-09 16:21:48 +0200

    Static attribute initialisert und Plappern Methode auf Wahr gesetzt

--- Klasse Datum/Klasse_Datum.cpp
+++ Klasse Datum/Klasse_Datum.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include "Klasse_Datum.h"
+using namespace std;
 
+bool Datum::Flag{ false };
 int Datum::KlassenZaehler{0};
 int main() {
+	
+	Datum::Plappern(true);
 
-	Datum heute{ 2,5,2022 };
-	Datum e1 {1, 1, 1};
-	Datum e2{ 1, 1, 1 };
-	Datum e3{ 1, 1, 1 };
-	Datum e4{ 1, 1, 1 };
-	Datum e5{ 1, 1, 1 };
-	Datum e6{ 1, 1, 1 };
-	Datum e7{ 1, 1, 1 };
-	Datum e8{ 1, 1, 1 };
-	Datum e9{ 1, 1, 1 };
-	Datum e10{ 1, 1, 1 };
-	std::cout << Datum::IstSchaltjahr(4) << std::endl;
-	std::cout << heute.IstSchaltjahr(100) << std::endl;
-	std::cout << heute.IstSchaltjahr(400) << std::endl;
-	std::cout << heute.TagDesJahres() << std::endl;
+		Datum heute{ 2,5,2020 };
+		Datum e1{ 1, 1, 1 };
+		Datum e2{ 1, 1, 1 };
+		Datum e3{ 1, 1, 1 };
+		Datum e4{ 1, 1, 1 };
+		Datum e5{ 1, 1, 1 };
+		Datum e6{ 1, 1, 1 };
+		Datum e7{ 1, 1, 1 };
+		Datum e8{ 1, 1, 1 };
+		Datum e9{ 1, 1, 1 };
+		Datum e10{ 1, 5, 1 };
 
-	system("pause");
-	return 0;
+		std::cout << Datum::IstSchaltjahr(4) << std::endl;
+		std::cout << heute.IstSchaltjahr(100) << std::endl;
+		std::cout << heute.IstSchaltjahr(400) << std::endl;
+		std::cout << heute.TagDesJahres() << std::endl;
+		std::cout << e9.TagDesJahres() << std::endl;
+		
+		system("pause");
+		return 0;
+	
 }
