169f90b17ef23ff1d1241a269af47a3e39e67383
Tom Brown <tom.brown@stud.hawk.de>
2022-05-19 13:36:30 +0200

    Testprogramm erweitert, damit alle Klassen getestet werden können.

--- Transportmittel/Transportmitteltest.cpp
+++ Transportmittel/Transportmitteltest.cpp
@@ -5,5 +5,19 @@ int main() {
 	cout << box.get_hoehe() << endl;
 	cout << box.get_breite() << endl;
 	box.bewegen("links");
+	
+	LandTransportmittel* box1 = new LandTransportmittel{ 2,3,2 };
+	box1->bewegen("Rechts");
+	box1->fahren(1000);
+	box1->schieben(10);
+
+
+	WasserTransportmittel* box2 = new WasserTransportmittel{ 5,10,15 };
+	box2->bewegen("Vorne");
+	box2->anlegen("Wilhelmshaven");
+	box2->ablegen("Amsterdam");
+
+	delete box1;
+	delete box2;
 	return 0;
 }
