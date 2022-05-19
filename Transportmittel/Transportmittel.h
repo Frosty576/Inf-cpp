a1141bc639ad7e9bd75b15ceceadb1d4fb5ef0c7
Tom Brown <tom.brown@stud.hawk.de>
2022-05-19 14:18:35 +0200

    Die Klassen Fahrrad, Rikscha und Auto werden von der Klasse LandTransportmittel abgeleitet. Fahhrad, Rikscha und Auto enthalten jeweils ein neue Methode. Rikscha und Auto überschreiben jeweils ein abgeleitete Methode.

--- Transportmittel/Transportmittel.h
+++ Transportmittel/Transportmittel.h
@@ -45,3 +45,47 @@ public:
 	}
 	double get_bruttoregistertonnen() const { return bruttoregistertonnen; }
 };
+
+class Fahrrad : public LandTransportmittel {
+private:
+	int anzahlgaenge;
+public:
+	Fahrrad(double Hoehe, double Breite, int Radzahl, int Anzahlgaenge) :LandTransportmittel(Hoehe, Breite, Radzahl), anzahlgaenge(Anzahlgaenge)
+	{}
+
+	void freihaendigfahren(string name) {
+		cout << name << " faehrt freihaendig" << endl;
+	}
+	int get_anzahlgaenge() const { return anzahlgaenge; }
+};
+
+class Rikscha : public LandTransportmittel {
+private:
+	string farbe;
+public:
+	Rikscha(double Hoehe, double Breite, int Radzahl, string Farbe) : LandTransportmittel(Hoehe, Breite, Radzahl), farbe(Farbe)
+	{}
+
+	void ziehen(int anzahlpersonen) {
+		cout << "Sie ziehen " << anzahlpersonen << " personen" << endl;
+	}
+	void schieben(double km) {
+		cout << "Sie muessen das Rikscha " << km << " km schieben" << endl;
+	}
+	string get_farbe() const { return farbe; }
+};
+
+class Auto : public LandTransportmittel {
+private:
+	double spritverbrauch;
+public:
+	Auto(double Hoehe, double Breite, int Radzahl, double Spritverbrauch) : LandTransportmittel(Hoehe, Breite, Radzahl), spritverbrauch(Spritverbrauch)
+	{}
+	void tanken(int liter) {
+		cout << "Sie Tanken " << liter << " liter" << endl;
+	}
+	void fahren(double km) {
+		cout << "Sie fahren " << km << " km mit dem Auto" << endl;
+	}
+	double get_spritverbrauch() const { return spritverbrauch; }
+};
