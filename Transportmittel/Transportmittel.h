bde4bc1fcdfee9969ec9ff9a035e6329c4ac1774
Tom Brown <tom.brown@stud.hawk.de>
2022-05-19 13:35:38 +0200

    Abgeleitete Klassen LandTransportmittel und WasserTransportmittel programmiert. Beide haben jeweils 1 extra Atrribute und zwei neue Methoden.

--- Transportmittel/Transportmittel.h
+++ Transportmittel/Transportmittel.h
@@ -8,12 +8,40 @@ private:
 	double breite;
 public:
 	Transportmittel(double Hoehe, double Breite) : hoehe(Hoehe), breite(Breite)
-	{
-		cout << "Allgemeiner konstruktor Transportmittel" << endl;
-	}
+	{}
 	void bewegen(string Richtung) {
 		cout << "In Richtung " << Richtung << " bewegen" << endl;
 	}
 	double get_hoehe() const { return hoehe; }
 	double get_breite() const { return breite; }
+};
+
+class LandTransportmittel : public Transportmittel {
+private:
+	int radzahl;
+public:
+	LandTransportmittel(double Hoehe, double Breite, int Radzahl) : Transportmittel(Hoehe, Breite), radzahl(Radzahl)
+	{}
+	void fahren(double km) {
+		cout << km << " km fahren" << endl;
+	}
+	void schieben(double km) {
+		cout << km << " km schieben" << endl;
+	}
+	int get_radzahl() const { return radzahl; }
+};
+
+class WasserTransportmittel : public Transportmittel {
+private:
+	double bruttoregistertonnen;
+public:
+	WasserTransportmittel(double Hoehe, double Breite, double Bruttoregistertonnen) : Transportmittel(Hoehe, Breite), bruttoregistertonnen(Bruttoregistertonnen)
+	{}
+	void anlegen(string Anlegehafen) {
+		cout << "Ihre Anlegehafen ist  " << Anlegehafen << endl;
+	}
+	void ablegen(string Ablegehafen) {
+		cout << "Ihre Ablegehafen ist  " << Ablegehafen << endl;
+	}
+	double get_bruttoregistertonnen() const { return bruttoregistertonnen; }
 };
