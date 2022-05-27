3038abc0bd3b3a7f48115f673efc8ee25e19d7aa
Tom Brown <tom.brown@stud.hawk.de>
2022-05-27 12:52:28 +0200

    Setter-methoden getestet. Ueberladene ausgabe- und eingabeoperatoren getestet.

--- Complex/complex.cpp
+++ Complex/complex.cpp
@@ -4,11 +4,21 @@
 int main() {
 	complex num1{ 2,4 };
 	complex num2{ 4,6 };
+	complex num3;
+	cin >> num3;
 	complex ergebnisAdd;
 	complex ergebnisSub;
 	ergebnisAdd = num1 + num2;
 	ergebnisSub = num1 - num2;
-	cout << "Die real Teil betraegt " << ergebnisAdd.get_real() << " und der Imaginaer teil betraegt " << ergebnisAdd.get_imaginaer() << "i" << endl;
-	cout << "Die real Teil betraegt " << ergebnisSub.get_real() << " und der Imaginaer teil betraegt " << ergebnisSub.get_imaginaer() << "i" << endl;
+	cout << ergebnisAdd.get_real() << "+" << ergebnisAdd.get_imaginaer() << "i" << endl;
+	cout << ergebnisSub.get_real() << ergebnisSub.get_imaginaer() << "i" << endl;
+	cout << num1 << endl;
+	num1.set_real(1);
+	num1.set_imaginaer(2);
+	cout << num1 << endl;
+	cout << num2 << endl;
+	cout << num3 << endl;
+	cout << ergebnisAdd << endl;
+	cout << ergebnisSub << endl;
 	return 0;
 }
