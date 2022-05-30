e759160db00a23e45f20b0325241203607d6c99d
Tom Brown <tom.brown@stud.hawk.de>
2022-05-30 12:08:23 +0200

    Attribute zu double gewechselt. Divisionsoperator wird ueberladen.

--- Complex/complex.h
+++ Complex/complex.h
@@ -1,28 +1,29 @@
 #pragma once
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
 class complex {
 private:
-	int real;
-	int imaginaer;
+	double real;
+	double imaginaer;
 public:
 	complex()
 	{
-		real = 0;
-		imaginaer = 0;
+		real = 0.0;
+		imaginaer = 0.0;
 	}
-	complex (int Real, int Imaginaer) : real(Real), imaginaer(Imaginaer)
+	complex (double Real, double Imaginaer) : real(Real), imaginaer(Imaginaer)
 	{}
 
-	int get_real() { return real; }
-	int get_imaginaer() { return imaginaer; }
+	double get_real() { return real; }
+	double get_imaginaer() { return imaginaer; }
 
-	void set_real(int r) {
+	void set_real(double r) {
 		real = r;
 	}
-	void set_imaginaer(int i) {
+	void set_imaginaer(double i) {
 		imaginaer = i;
 	}
 	complex operator+ (const complex& b) {
@@ -37,12 +38,27 @@ public:
 		complexnum.imaginaer = this->imaginaer - b.imaginaer;
 		return complexnum;
 	}
+	complex operator/ (const complex& b) {
+		complex complexnum;
+		try {
+			if (b.real == 0 && b.imaginaer == 0) {
+				throw overflow_error("Invalid Argument");
+			}
+			complexnum.real = ((this->real * b.real) + (this->imaginaer * b.imaginaer)) / (pow(this->imaginaer, 2) + pow(b.imaginaer, 2));
+			complexnum.imaginaer = ((this->imaginaer * b.real) - (this->real * b.imaginaer)) / (pow(b.real, 2) + pow(b.imaginaer, 2));		
+		}
+		catch(overflow_error& e) {
+			cout << e.what() << " " << this->real << "+" << this ->imaginaer  << "i / " << b.real << "+" << b.imaginaer << "i -> ";
+		}
+		return complexnum;
+	}
+
 	friend ostream& operator<< (ostream& os, complex& num);
 	friend istream& operator>> (istream& is, complex& num);
 };
 
 ostream& operator<< (ostream& os, complex& num) {
-	if (num.imaginaer >= 0) {
+	if (num.imaginaer >= 0.0) {
 		os << num.real << "+" << num.imaginaer << "i";
 		return os;
 	}
